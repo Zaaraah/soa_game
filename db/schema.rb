13ea602c95240bf9267e246182c1ea51daeb3fba
Denise <Denise@Denises-MacBook-Pro.local>
2015-03-24 23:48:33 +0800

    Yay pretty login

--- db/schema.rb
+++ db/schema.rb
@@ -56,8 +56,6 @@ ActiveRecord::Schema.define(version: 20150324142512) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "username"
-    t.string   "handle"
-    t.string   "mobile_number"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
